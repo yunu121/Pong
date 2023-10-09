aab63e757ee727474d28e466079ecc56fd4f961b
Owen <>
2023-10-10 10:49:31 +1300

    Added ir send and recv functions

--- game.c
+++ game.c
@@ -1,9 +1,24 @@
 #include "system.h"
+#include "ir_uart.h"
+
+char recv_signal(void)
+{
+    char c;
+    if (ir_uart_read_ready_p()) {
+        c = ir_uart_getc();
+    }
+    return c;
+}
+
+void send_signal(char c)
+{
+    ir_uart_putc(c);
+}
 
 int main (void)
 {
     system_init ();
-
+    ir_uart_init ();
 
     while (1)
     {
