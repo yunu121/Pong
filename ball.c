38c2f359b7bd89f1f15b5b9a4a1d1f7fb6414148
Owen <>
2023-10-10 10:22:27 +1300

    Added ball functions to set its attributes

---
+++ ball.c
@@ -0,0 +1,32 @@
+/* @file   ball.c
+ * @author Yunu Cho, Owen Zhou
+ * @date   10 October 2023
+ * @brief  Declaration and definitions of the ping pong ball
+ */
+
+#include "ball.h"
+
+/* Ball initialisation and returns ball object */
+Ball_t ball_init(void)
+{
+    Ball_t ball;
+    ball.x = 0;
+    ball.y = 0;
+    ball.vx = 0;
+    ball.vy = 0;
+    return ball;
+}
+
+/* Change the xy position of the ball */
+void ball_set_pos(Ball_t ball, uint8_t x, uint8_t y)
+{
+    ball.x = x;
+    ball.y = y;
+}
+
+/* Change the vxy direction of the ball*/
+void ball_set_dir(Ball_t ball, int8_t vx, int8_t vy)
+{
+    ball.vx = x;
+    ball.vy = y;
+}
